1762e875280ac807a58c065cb93a03256ee44e73
roniketraut <roniketraut@gmail.com>
2025-06-07 13:29:13 +0545

    Add dbt project files and initial structure

--- target/compiled/product_warehouse_dbt/models/marts/dim_products.sql
+++
@@ -1,8 +0,0 @@
-select
-    PRODUCT_ID,
-    TITLE,
-    PRICE,
-    CATEGORY,
-    RATING,
-    RATING_COUNT
-from FAKESTORE_DEV_DB.DBT_MODELS_staging_layer.stg_products
--- target/compiled/product_warehouse_dbt/models/marts/dim_users.sql
+++
@@ -1,6 +0,0 @@
-select
-    USER_ID,
-    USERNAME,
-    EMAIL,
-    ADDRESS_CITY
-from FAKESTORE_DEV_DB.DBT_MODELS_staging_layer.stg_users
